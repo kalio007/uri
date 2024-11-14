aa592889e32957543bb9173e034babcdfed032a0
kalio007 <kalioprince1@gmail.com>
2024-11-14 11:44:34 +0100

    added statistics cahrt

--- frontend/components/ApplicationStatistics.tsx
+++ frontend/components/ApplicationStatistics.tsx
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Pie } from 'react-chartjs-2';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+
+// Register the required Chart.js components
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const ApplicationStatistics = () => {
   const [stats, setStats] = useState({ total: 0, countsByStatus: {} });
 
   useEffect(() => {
-    axios.get('http://localhost:3000/applications/stats')
-      .then(response => setStats(response.data))
-      .catch(error => console.error(error));
+    axios
+      .get("http://localhost:3001/applications/stats")
+      .then((response) => setStats(response.data))
+      .catch((error) => console.error(error));
   }, []);
 
   const data = {
@@ -16,7 +23,7 @@ const ApplicationStatistics = () => {
     datasets: [
       {
         data: Object.values(stats.countsByStatus),
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
       },
     ],
   };
